9e0f322f18d075a5adf46a4f870e4638614f6951
Mayté Mejía <maytemejia@live.com.mx>
2023-06-25 20:14:29 -0500

    add test when a owner is not found

--- spec/requests/api/v1/owners_spec.rb
+++ spec/requests/api/v1/owners_spec.rb
@@ -34,12 +34,19 @@ RSpec.describe('Api::V1::Owners', type: :request) do
     context 'successfull response' do
       before { get "/api/v1/owners/#{Owner.first.id}" }
 
-      it 'returns http success' do
+      it 'returns http success response' do
         expect(response).to have_http_status(:success)
       end
 
       it 'return specific data from register owner' do
         expect(response.body).to match(expect_response.to_json)
+      end
+    end
+
+    context 'when the owner is not found' do
+      it 'return http bad request' do
+        get '/api/v1/owners/10000'
+        expect(response).to have_http_status(400)
       end
     end
   end
